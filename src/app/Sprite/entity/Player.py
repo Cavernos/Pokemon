e1367fc2897322f27e68129a45ec294fa2f03654
Cavernos <louisdescavernes@gmail.com>
2024-11-04 17:39:49 +0100

    Add animation to player

--- src/app/Sprite/entity/Player.py
+++ src/app/Sprite/entity/Player.py
@@ -15,22 +15,27 @@ class Player(pygame.sprite.Sprite):
         self.height = self.loaded_image.get_height() // 4
         self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
         self.image = self.loaded_image.subsurface((4, 6, self.width, self.height))
+        self.animation_counter = [0 for i in range(4)]
         self.velocity = 1
 
     def move_left(self, x):
-        self.image = self.loaded_image.subsurface((0, self.height, self.width, self.height))
+        self.image = self.loaded_image.subsurface(
+            (self.play_anim(0)*self.width, self.height, self.width, self.height))
         self.rect.x -= (x * self.velocity)
 
     def move_right(self, x):
-        self.image = self.loaded_image.subsurface((0, 2*self.height, self.width, self.height))
+        self.image = self.loaded_image.subsurface(
+            (self.play_anim(1) * self.width, 2*self.height, self.width, self.height))
         self.rect.x += (x * self.velocity)
 
     def move_bottom(self, y):
-        self.image = self.loaded_image.subsurface((0, 0, self.width, self.height))
+        self.image = self.loaded_image.subsurface(
+            (self.play_anim(2) * self.width, 0 * self.height, self.width, self.height))
         self.rect.y += (y * self.velocity)
 
     def move_up(self, y):
-        self.image = self.loaded_image.subsurface((0, 3*self.height, self.width, self.height))
+        self.image = self.loaded_image.subsurface(
+            (self.play_anim(3) * self.width, 3 * self.height, self.width, self.height))
         self.rect.y -= (y * self.velocity)
 
     def sprint(self, multiplier):
@@ -38,3 +43,10 @@ class Player(pygame.sprite.Sprite):
 
     def slow(self):
         self.velocity = 1
+
+    def play_anim(self, counter_index):
+        self.animation_counter[counter_index] += (self.velocity / 2)
+        if self.animation_counter[counter_index] >= 4:
+            self.animation_counter[counter_index] = 0
+        return int(self.animation_counter[counter_index])
+
