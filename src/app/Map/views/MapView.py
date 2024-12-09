d0a651d5798e305138632cffbd879269fdf575b1
Nicolas <nico21.clement@gmail.com>
2024-12-09 16:01:33 +0100

    save

--- src/app/Map/views/MapView.py
+++ src/app/Map/views/MapView.py
@@ -13,10 +13,17 @@ from lib.widgets.Label import Label
 
 class MapView(TiledView, ABC):
     def __init__(self, screen):
+        """
+        _summary_
+
+        Args:
+            screen (pygame): _description_
+        """
         super().__init__(screen)
         self.map_layer.zoom = 5
         self.obstacles = []
 
+        # quit option
         self.accept_button = Button(self.screen,0,70, 30, 30, name='yes')
         self.discard_button = Button(self.screen,270,70,30, 30, name='no')
         self.quit_label = Label(self.screen, 0, 100 / 2 - 20, 100, 20, name='Do you want to quit ?', color='#ffffff')
@@ -24,10 +31,13 @@ class MapView(TiledView, ABC):
         self.quit_menu = Menu(self.screen, self.screen.get_rect().centerx - 150, self.screen.get_rect().centery - 50, 300, 100, self.accept_button, self.discard_button, self.quit_label)
         self.quit_menu.set_alpha(0)
         self.hitbox_shown = False
+        
+        # collide
         for objs in self.tmx_data.objectgroups:
             for obj in objs:
                 if objs.name == 'collision':
                     self.obstacles.append(pygame.Rect(obj.x, obj.y, obj.width, obj.height))
+        
         if Container.exists(Sprite.__name__):
             self.player = Player(71, 84)
             self.pokemon = Container.get(LoaderInterface.__name__).load_from_index(Pokemon, 1)
@@ -37,6 +47,9 @@ class MapView(TiledView, ABC):
             self.entities = self.group.sprites()
 
     def update(self):
+        """
+        Main loop function that refreshes the map.
+        """
         if Container.exists(Sprite.__name__):
             #self.pokemon.update()
             if self.player.rect.colliderect(self.pokemon):
@@ -45,6 +58,9 @@ class MapView(TiledView, ABC):
             pygame.event.post(pygame.event.Event(pygame.USEREVENT, key=pygame.key.get_pressed()))
 
     def render(self):
+        """
+        
+        """
         if Container.exists(Sprite.__name__):
             self.group.center(self.player.rect.center)
             self.group.draw(self.screen)
@@ -52,5 +68,3 @@ class MapView(TiledView, ABC):
             self.map_layer.center((71 * 16, 84 * 16))
             self.map_layer.draw(self.screen, self.screen.get_rect())
         self.quit_menu.render()
-
-
